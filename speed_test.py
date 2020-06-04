0d65944335f8faf5ec794872ea6bc45498c569bd
Beaumont Steele <steele.b97@gmail.com>
2020-06-04 19:21:44 +0200

    compare version1 done

--- speed_test.py
+++ speed_test.py
@@ -8,24 +8,33 @@ def print_out_random_line(random_sentence):
     print(random_sentence)
 
 
-def split_sentence(random_sentence):   
+def split_sentence(random_sentence):
     lst_random_sentence = list(random_sentence)
-    print(lst_random_sentence)
-   
+    return lst_random_sentence
+    # print(lst_random_sentence)
+
 
 def typing_input():
     input_1 = input("Enter sentence:")
     input_split = list(input_1)
-    print(input_split)
-    
+    return input_split
+    # print(input_split)
+
+
+def get_win(return_value_2, return_value):
+    if return_value == return_value_2:
+        print("Fuck yeah")
+    else:
+        print("Sorry Bro")
 
 
 def main():
     lines = open('sentences.txt', 'r').read().splitlines()
     random_sentence = random.choice(lines)
     print_out_random_line(random_sentence)
-    typing_input()
-    split_sentence(random_sentence)
+    return_value = typing_input()
+    return_value_2 = split_sentence(random_sentence)
+    get_win(return_value_2, return_value)
     #x = print_out_random_line()
     #x.split()
     #print(x)
